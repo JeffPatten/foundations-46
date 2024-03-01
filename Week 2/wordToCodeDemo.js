732a153f18a12d6f8e7950127a699bd0ccf9fd3b
JeffPatten <jeffspatten@gmail.com>
2024-03-01 17:04:14 -0600

    added custom demo code to week 2

---
+++ Week 2/wordToCodeDemo.js
@@ -0,0 +1,15 @@
+// Write a function called damageAndDebuff that
+// can take in `healthTotal`,`baseDamage`,
+// and `healthDebuff` arguments.
+
+// Inside the function, subtract
+// the value of the base damage from the health total, 
+// and update the total to that value. 
+// Calculate the healthDebuff and 
+// subtract it from the total. 
+// Return the new healthTotal.  
+
+// Note: the numbers passed in for `healthDebuff` will be
+// decimals, for example: .15 for a 15% health debuff.
+
+
